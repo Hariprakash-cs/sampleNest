faf49d84984c58f1b4ae3a2256a4699bb8f0a0a0
Hariprakash-cs <hari.prakash@contentstack.com>
2024-11-14 10:27:49 +0530

    add

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const logger = new Logger('bootstrap');
+  const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3030);
-  logger.log('Application listening on port 3000');
+  logger.log("Application listening on port 3000");
 }
 bootstrap();
