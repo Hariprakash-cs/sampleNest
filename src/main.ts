16d1ae04fba76489943eca495e6b7f258dfa5a4c
Hariprakash-cs <hari.prakash@contentstack.com>
2024-11-14 12:16:03 +0530

    add

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { Logger, ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   console.log("Hello World");
-  const unusedVariable = "This variable is not used";
+  // const unusedVariable = "This variable is not used";
   await app.listen(3030);
   logger.log("Application listening on port 3000");
 }
