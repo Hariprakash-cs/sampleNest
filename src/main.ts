7104e3f05a06074c84687da3d9096f5943796d72
Hariprakash-cs <hari.prakash@contentstack.com>
2024-11-14 12:01:47 +0530

    add

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   console.log("Hello World");
-  // const unusedVariable = "This variable is not used";
+  const unusedVariable = "This variable is not used";
   await app.listen(3030);
   logger.log("Application listening on port 3000");
 }
